f3542f23eae56fc14ea2b94ce9e0ef9ac82312ef
Dan Rosu <cristian.rosu@student.upt.ro>
2023-11-02 17:25:26 +0200

    commit1234

--- src/ECU1/ECU1.hpp
+++ src/ECU1/ECU1.hpp
@@ -3,48 +3,49 @@
 #include "ECU2.hpp"
 using namespace std;
 
-class ECU1{
-    public: ECU1(string model, int ecuNumber, string communicationType, string position, int soft, int ecuPair);
+class ECU1
+{
+public:
+    ECU1(string model, int ecuNumber, string communicationType, string position, int soft, int ecuPair);
 
-    ECU1(cont ECU1 &ecu1);
+    ECU1(const ECU1 &ecu1);
 
-    ECU1& operator = (const Plane& other);
+    ECU1 &operator=(const ECU1 &other);
 
-    ~Plane();
-
-    private:
-    string model;
-    int ecuNumber;
-    string communicationType;
-    string position;
-    int soft;
-    int ecuPair;
-     
-    ECU2 *ecu2;
+    ~ECU1();
 
     void setModel(
-        string model
-    );
+        string model);
 
     void setEcuNumber(
-        int ecuNumber
-    );
-    
+        int ecuNumber);
+
     void setCommunicationType(
-        string communicationType
-    );
+        string communicationType);
 
     void setPosition(
-        string position
-    );
+        string position);
 
     void setSoft(
-        int soft
-    );
+        int soft);
+
+    void setEcuPair(
+        int ecuPair);
 
     string getModel();
     int getEcuNumber();
     string getCommunicationType();
     string getPosition();
     int getSoft();
-}
+    int getEcuPair();
+
+    ECU2 *ecu2;
+
+private:
+    string model;
+    int ecuNumber;
+    string communicationType;
+    string position;
+    int soft;
+    int ecuPair;
+};
